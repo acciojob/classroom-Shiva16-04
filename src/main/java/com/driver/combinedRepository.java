8cc34427fbc7e4e65e1bcf927078fdfb91106663
Shiva16-04 <shivakumaryedla1999@gmail.com>
2023-10-05 18:36:29 +0530

    commit1

---
+++ src/main/java/com/driver/combinedRepository.java
@@ -0,0 +1,79 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class combinedRepository {
+    private HashMap<String, Student>studentsDatabase;
+    private HashMap<String, Teacher>teachersDatabase;
+    private HashMap<String, List<Student>>pairDatabase;
+    combinedRepository(){
+        studentsDatabase=new HashMap<>();
+        teachersDatabase=new HashMap<>();
+        pairDatabase=new HashMap<>();
+    }
+
+    public HashMap<String, Student> getStudentsDatabase() {
+        return studentsDatabase;
+    }
+
+    public HashMap<String, Teacher> getTeachersDatabase() {
+        return teachersDatabase;
+    }
+
+    public void setStudentsDatabase(HashMap<String, Student> studentsDatabase) {
+        this.studentsDatabase = studentsDatabase;
+    }
+
+    public void setTeachersDatabase(HashMap<String, Teacher> teachersDatabase) {
+        this.teachersDatabase = teachersDatabase;
+    }
+
+    public HashMap<String, List<Student>> getPairDatabase() {
+        return pairDatabase;
+    }
+
+    public void setPairDatabase(HashMap<String, List<Student>> pairDatabase) {
+        this.pairDatabase = pairDatabase;
+    }
+
+    //Method 1: add Student
+    public String addStudent(Student student){
+        if(getStudentsDatabase().containsKey(student)==false) {
+            getStudentsDatabase().put(student.getName(), student);
+            return "New student added successfully";
+        }else{
+            return "New student not added successfully";
+        }
+    }
+
+    //Method 2: add Teacher
+    public String addTeacher(Teacher teacher){
+        if(getTeachersDatabase().containsKey(teacher.getName())==false) {
+            getTeachersDatabase().put(teacher.getName(), teacher);
+            getPairDatabase().put(teacher.getName(), new ArrayList<>());
+            return "New teacher added successfully";
+        }else{
+            return "New teacher not added successfully";
+        }
+    }
+
+    //Method 3: add Student and teacher
+    public String addStudentTeacherPair(String student, String teacher){
+        if(getTeachersDatabase().containsKey(teacher)==true && getStudentsDatabase().containsKey(student)==true){
+            getPairDatabase().get(teacher).add(studentsDatabase.get(student));
+            return "New student-teacher pair added successfully";
+        }
+        else{
+            return "New student-teacher pair not added successfully";
+        }
+
+    }
+
+
+}
---
+++ src/main/java/com/driver/combinedService.java
@@ -0,0 +1,95 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class combinedService {
+    @Autowired
+    public combinedRepository repoObj;
+
+    //Method 1: add Student
+    public String addStudent(Student student){
+        return repoObj.addStudent(student);
+    }
+
+    //Method 2: add Teacher
+    public String addTeacher(Teacher teacher){
+        return repoObj.addTeacher(teacher);
+    }
+
+    //Method 3: add Student teacher pair
+    public String addStudentTeacherPair(String student, String teacher){
+        return repoObj.addStudentTeacherPair(student, teacher);
+    }
+
+    //Method 4: get student by name
+    public Student getStudentByName(String student){
+        HashMap<String, Student>studentDb=repoObj.getStudentsDatabase();
+        if(studentDb.containsKey(student)==true){
+            return studentDb.get(student);
+        }else{
+            return null;
+        }
+    }
+
+    //Method 5: get teacher by name
+    public Teacher getTeacherByName(String teacher){
+        HashMap<String,Teacher>teacherDb=repoObj.getTeachersDatabase();
+        if(teacherDb.containsKey(teacher)==true){
+            return teacherDb.get(teacher);
+        }else{
+            return null;
+        }
+    }
+
+    //Method 6: get students by teacher name
+    public List<String> getStudentsByTeacherName(String teacher){
+        HashMap<String, List<Student>>pairDb=repoObj.getPairDatabase();
+        if(pairDb.containsKey(teacher)){
+            List<String>students=new ArrayList<>();
+            for(Student student:pairDb.get(teacher))students.add(student.getName());
+            return students;
+        }else{
+            return new ArrayList<>();
+        }
+    }
+
+    //Method 7: get All Students names
+    public List<String> getAllStudents(){
+        HashMap<String, Student>studentDb=repoObj.getStudentsDatabase();
+        if(studentDb!=null){
+            List<String>studentsNames=new ArrayList<>();
+            for(String name:studentDb.keySet()){
+                studentsNames.add(name);
+            }
+            return studentsNames;
+        }else {
+            return new ArrayList<>();
+        }
+    }
+
+    //Method 8: delete Teacher By Name
+    public String deleteTeacherByName(String teacher){
+        HashMap<String,Teacher>teacherDb=repoObj.getTeachersDatabase();
+        HashMap<String, List<Student>>pairDb=repoObj.getPairDatabase();
+        if(teacherDb==null || teacherDb.size()==0 ||teacherDb.containsKey(teacher)==false)return " teacher not found";
+        teacherDb.remove(teacher);
+        if(pairDb.containsKey(teacher)==true)pairDb.remove(teacher);
+        return " removed successfully";
+    }
+
+    //Method 9: deleteAllTeachers
+    public String deleteAllTeachers(){
+        HashMap<String,Teacher>teacherDb=repoObj.getTeachersDatabase();
+        HashMap<String, List<Student>>pairDb=repoObj.getPairDatabase();
+        if(teacherDb==null ||teacherDb.size()==0)return " teachers not found";
+        teacherDb.clear();
+        pairDb.clear();
+        return "All teachers deleted successfully";
+    }
+}
